5e440ef486c1245beabfe4b34807ac2c18bd02b7
irazramin <irazramin@gmail.com>
2022-04-29 20:53:10 +0600

    delete api added

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const { MongoClient, ServerApiVersion } = require('mongodb');
 const port = process.env.PORT || 5000;
 const cors = require('cors');
 require('dotenv').config();
+const ObjectId = require('mongodb').ObjectId;
 
 // middleware
 app.use(express.json());
@@ -17,26 +18,30 @@ const client = new MongoClient(uri, {
 });
 
 async function run() {
+  await client.connect();
+  const userCollection = client
+    .db('warehouseDb')
+    .collection('productCollections');
   try {
-    await client.connect();
-      const userCollection = client
-        .db('warehouseDb')
-        .collection('productCollections');
 
-app.get('/inventory',async (req, res) => {
-  const query = {}
-  const cursor = userCollection.find(query);
-  const result = await cursor.toArray();
-
-  res.send(result)
-});
+    app.get('/inventory', async (req, res) => {
+      const query = {};
+      const cursor = userCollection.find(query);
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+    app.get('/inventory/:id', async (req, res) => {
+      const id = req.params.id;
+      console.log(id)
+    const query = { _id: ObjectId(id) };
+      const result = await userCollection.findOne(query)
+      res.send(result);
+    });
   } finally {
   }
 }
 
 run().catch(console.dir);
-
-
 
 app.listen(port, () => {
   console.log('App is listening ');
