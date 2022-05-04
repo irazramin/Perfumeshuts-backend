b708c989bc00dd37a866f68427a0771e085b3488
irazramin <irazramin@gmail.com>
2022-05-04 19:38:16 +0600

    simple commit

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 const express = require('express');
+const app = express();
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const port = process.env.PORT || 5000;
 const cors = require('cors');
-const jwt = require('jsonwebtoken');
 require('dotenv').config();
 const ObjectId = require('mongodb').ObjectId;
-
-const app = express();
+const jwt  = require('jsonwebtoken')
 // middleware
 app.use(cors());
 app.use(express.json());
@@ -44,40 +43,41 @@ async function run() {
     });
     app.get('/user', async (req, res) => {
       const email = req.query.email;
-      const query = { email: email };
+      const query = {email:email};
       const cursor = productCollection.find(query);
       const result = await cursor.toArray();
       res.send(result);
     });
 
-    app.get('/inventory/:id', async (req, res) => {
-      const id = req.params.id;
-      const query = { _id: ObjectId(id) };
-      const cursor = productCollection.find(query);
-      const result = await cursor.toArray();
-      res.send(result);
-    });
-    app.put('/inventory/:id', async (req, res) => {
-      const id = req.params.id;
-      const updatedItem = req.body;
-      const filter = { _id: ObjectId(id) };
-      const option = { upsert: true };
-      const update = {
-        $set: {
-          quantity: updatedItem.quantity,
-        },
-      };
-      const result = await productCollection.updateOne(filter, update, option);
-      res.send(result);
-    });
-
-    app.post('/login', async (req, res) => {
-      const user = req.body;
-      const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN, {
-        expiresIn: '1d',
+      app.get('/inventory/:id', async (req, res) => {
+        const id = req.params.id;
+        const query = { _id: ObjectId(id) };
+        const cursor =  productCollection.find(query);
+        const result = await cursor.toArray();
+         res.send(result);
       });
-      res.send({ accessToken });
-    });
+         app.put('/inventory/:id', async (req, res) => {
+           const id = req.params.id;
+           const updatedItem = req.body;
+           const filter = { _id: ObjectId(id) };
+           const option = {upsert : true}
+           const update = {
+             $set:{
+                quantity:updatedItem.quantity,
+             }
+           }
+           const result = await productCollection.updateOne(filter,update,option);
+           res.send(result);
+         });
+
+           app.post('/login', async (req, res) => {
+             const user = req.body;
+             const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN, {
+               expiresIn: '1d',
+             });
+             res.send({ accessToken });
+           });
+
   } finally {
   }
 }
